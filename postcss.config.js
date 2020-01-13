b80333396a677b14cc503a1ebd59740af75a1d0a
Mikko Forsström <pekkis@Mikkos-MacBook-Pro.local>
2020-01-13 15:27:27 +0200

    Removado.

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {},
-    "postcss-advanced-variables": {},
-    "postcss-nested": {},
-    autoprefixer: {}
-  }
-};
