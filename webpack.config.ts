24657624d7c3e2433ed5ca48bf151d7668bba48d
Mikko Forsström <pekkisx@gmail.com>
2020-08-01 11:59:21 +0300

    Upgraded deps.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,32 +1,47 @@
-import broilerplate, {
-  pathsFromRootPath,
-  addEntrypoint,
-  build,
-  setContext
-} from "@dr-kobros/webpack-broilerplate";
-
-import babel from "@dr-kobros/webpack-broilerplate/dist/features/babel";
-import mjs from "@dr-kobros/webpack-broilerplate/dist/features/mjs";
-// import CoreJSPlugin from "corejs-upgrade-webpack-plugin";
-
-import images from "@dr-kobros/webpack-broilerplate/dist/features/images";
-import html from "@dr-kobros/webpack-broilerplate/dist/features/html";
-import copyFiles from "@dr-kobros/webpack-broilerplate/dist/features/copyFiles";
-import saneDefaultOptions from "@dr-kobros/webpack-broilerplate/dist/features/saneDefaultOptions";
-import emotion from "@dr-kobros/webpack-broilerplate-emotion";
-
-import { pipe, over, lensPath, append } from "ramda";
+// import { pipe, over, lensPath, append } from "ramda";
 import path from "path";
 import util from "util";
+
+import { any, pickBy, mapObjIndexed } from "ramda";
 
 // import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 import pkg from "./package.json";
-import { Configuration } from "webpack-dev-server";
+// import { Configuration } from "webpack-dev-server";
 
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
-const addBundleAnalyzer = (config: Configuration, mode: string) => {
+import * as webpack from "webpack";
+// import * as webpackDevServer from "webpack-dev-server";
+
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import CaseSensitivePathsPlugin from "case-sensitive-paths-webpack-plugin";
+import WatchMissingNodeModulesPlugin from "react-dev-utils/WatchMissingNodeModulesPlugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const hasPrefix = (prefixes: string[], value: string): boolean => {
+  return any(p => value.startsWith(p), prefixes);
+};
+
+const getEnvironmentVariables = (
+  env: NodeJS.ProcessEnv,
+  prefix: string[],
+  whitelisted: string[]
+): { [key: string]: string } => {
+  // console.log(env, "env");
+
+  const picked = pickBy(
+    (v, k) =>
+      k === "NODE_ENV" || whitelisted.includes(v) || hasPrefix(prefix, k),
+    env
+  );
+
+  return mapObjIndexed(v => {
+    return JSON.stringify(v);
+  }, picked);
+};
+
+const getBundleAnalyzer = (mode: string) => {
   const options: BundleAnalyzerPlugin.Options =
     mode === "development"
       ? {}
@@ -37,109 +52,117 @@ const addBundleAnalyzer = (config: Configuration, mode: string) => {
         };
 
   const p = new BundleAnalyzerPlugin(options);
-  return over(lensPath(["plugins"]), append(p), config);
+  return p;
 };
 
 const mode =
   process.env.NODE_ENV === "development" ? "development" : "production";
-const bp = broilerplate(mode, pathsFromRootPath(__dirname), {
-  debug: true
-});
 
-/*
-const externalCSS = () => (bp: BroilerplateContext): BroilerplateContext => {
-  const Plugin = new MiniCssExtractPlugin();
-
-  const ruleset: RuleDefinition = {
-    factory: () => {
-      return {
-        test: /\.css$/,
+const c: webpack.Configuration = {
+  mode,
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  devtool: false,
+  output: {
+    publicPath: "/"
+  },
+  devServer: {
+    port: 11000,
+    hot: true,
+    index: "index.html",
+    disableHostCheck: true,
+    historyApiFallback: true
+  },
+  resolve: {
+    modules: ["/home/pekkis/js/mhm-2000-remake/node_modules"],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".mjs"]
+  },
+  context: "/home/pekkis/js/mhm-2000-remake/src",
+  entry: { client: "./client.tsx" },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEVELOPMENT__: mode === "development",
+      __PRODUCTION__: mode === "production",
+      "process.env": getEnvironmentVariables(process.env, ["REACT_APP_"], [])
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "assets/web", flatten: false }]
+    }),
+    new CaseSensitivePathsPlugin(),
+    new WatchMissingNodeModulesPlugin(path.resolve("node_modules")),
+    new HtmlWebpackPlugin({
+      template: "assets/index.html",
+      favicon: "assets/index.html",
+      chunksSortMode: "auto",
+      title: "MHM 2000"
+    }),
+    getBundleAnalyzer(mode)
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        include: ["/home/pekkis/js/mhm-2000-remake/src"],
         use: [
           {
-            loader:
-              bp.mode === "production"
-                ? MiniCssExtractPlugin.loader
-                : "style-loader"
+            loader: "file-loader",
+            options: { name: "[path][name]-[hash].[ext]", emitFile: true }
           },
+          { loader: "img-loader", options: { enabled: mode === "production" } }
+        ]
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: [
           {
-            loader: "css-loader"
+            loader: "babel-loader",
+            options: {
+              babelrc: false,
+              presets: [
+                "@babel/preset-typescript",
+                [
+                  "@babel/preset-env",
+                  {
+                    debug: true,
+                    useBuiltIns: "usage",
+                    targets: {
+                      browsers: pkg.browserslist[mode]
+                    },
+                    modules: false,
+                    corejs: 3
+                  }
+                ],
+                ["@babel/preset-react", { development: true }],
+                "@emotion/babel-preset-css-prop"
+              ],
+              plugins: [
+                "@babel/plugin-syntax-dynamic-import",
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-proposal-nullish-coalescing-operator",
+                "@babel/plugin-proposal-optional-chaining"
+              ],
+              cacheDirectory: true
+            }
           }
         ],
-        include: [bp.paths.modules, bp.paths.src]
-      };
-    }
-  };
-
-  return pipe(
-    whenProduction(addPlugin(createPluginDefinition(() => Plugin))),
-    addRule(ruleset)
-  )(bp);
-};
-*/
-
-/*
-const coreJS = () =>
-  addPlugin(
-    createPluginDefinition(() => CoreJSPlugin({ resolveFrom: [process.cwd()] }))
-  );
-*/
-
-const bp2 = pipe(
-  saneDefaultOptions(),
-  copyFiles("assets/web"),
-  html({}),
-  images(),
-  setContext(path.resolve(__dirname, "./src")),
-  addEntrypoint("client", "./client.tsx"),
-  babel({ browsers: pkg.browserslist[mode] }),
-  mjs(),
-  // coreJS(),
-  // externalCSS(),
-  emotion()
-)(bp);
-const config = build(bp2);
-
-const config2 = config;
-
-/*
-const config2 = over(
-  lensPath(["module", "rules", 1, "exclude", 0]),
-  () => {
-    return /node_modules\/(?!react-country-flags)/;
-  },
-  config
-);
-*/
-
-if (config2.output) {
-  config2.output.publicPath = "/";
-} else {
-  config2.output = {
-    publicPath: "/"
-  };
-}
-
-config2.devServer = {
-  port: 11000,
-  hot: true,
-  index: "index.html",
-  disableHostCheck: true,
-  historyApiFallback: true
+        exclude: ["/home/pekkis/js/mhm-2000-remake/node_modules"]
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      }
+    ]
+  }
 };
 
-const config3 = over(
-  lensPath(["module", "rules", 1, "use", 0, "options"]),
-  options => {
-    return {
-      ...options,
-      cacheDirectory: true
-    };
-  },
-  config2
-);
+console.log(util.inspect(c, false, 999));
 
-const withBundleAnalyzer = addBundleAnalyzer(config3, mode);
+export default c;
 
-console.log(util.inspect(withBundleAnalyzer, false, 999));
+// process.exit();
 
-export default withBundleAnalyzer;
+// export default withBundleAnalyzer;
